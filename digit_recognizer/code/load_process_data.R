31a07753fe991e1c473e14b717227d8ec4426cc0
Eric Peng <haoericpeng@users.noreply.github.com>
2016-01-21 22:09:22 -0800

    Updated data processing script

--- digit_recognizer/code/load_process_data.R
+++ digit_recognizer/code/load_process_data.R
@@ -1,7 +1,15 @@
 library(readr)
 
 # Read data
-trainData <- read_csv("train.csv")
-testFeatures <- read_csv("test.csv")
+trainData <- read_csv("../data/train.csv")
+testFeatures <- read_csv("../data/test.csv")
 trainLabels <- as.factor(trainData[, 1])
 trainFeatures <- trainData[, -1]
+
+# Remove features that take on the same value for all training and test samples
+uselessIndices <- which(apply(rbind(trainFeatures, testFeatures),
+                              2,
+                              function(x) length(unique(x))) == 1)
+uselessIndices <- as.vector(uselessIndices)
+trainFeatures <- trainFeatures[, -uselessIndices]
+testFeatures <- testFeatures[, -uselessIndices]
